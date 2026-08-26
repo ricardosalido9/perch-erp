a99a00c41846e4de7bc6f58fae34ebea78002c6a
ricardosalido9 <ricardo.salido@grupocfa.com>
2026-08-26 17:09:58 -0600

    Add files via upload

--- api/erp.js
+++ api/erp.js
@@ -45,6 +45,7 @@ const RUTAS = {
   'nomina-pdf':   require('../lib/handlers/nomina-pdf'),
   'banco-import': require('../lib/handlers/banco-import'),
   'banco-aprobar':require('../lib/handlers/banco-aprobar'),
+  'banco-cuentas':require('../lib/handlers/banco-cuentas'),
   emitidos:       require('../lib/handlers/emitidos'),
   'relacion-pdf': require('../lib/handlers/relacion-pdf'),
   'envios-folio': require('../lib/handlers/envios-folio'),
